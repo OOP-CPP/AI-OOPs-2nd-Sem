48fef505f71cf405bad46850538aa43242e71802
Arnav Jha <91931013+left01205@users.noreply.github.com>
2024-05-09 23:13:30 +0530

    Update Calender.cpp

--- Assisgnments/Submissions/10_AI_ARNAV_JHA/Calender.cpp
+++ Assisgnments/Submissions/10_AI_ARNAV_JHA/Calender.cpp
@@ -1,57 +1,63 @@
-#include <iostream>
-#include <iomanip>
+#include<iostream>
 using namespace std;
-// To check if the given year is a leap year
-bool isLeapYear(int year) {
-    return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+int dayNumber(int day, int month, int year){
+   static int t[] = { 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 };
+   year -= month < 3;
+   return ( year + year/4 - year/100 + year/400 + t[month-1] + day) % 7;
 }
-// To get the number of days in a month
-int getDaysInMonth(int month, int year) {
-    if (month == 2)
-    {
-        return isLeapYear(year) ? 29 : 28;
-    }
-    else if (month == 4 || month == 6 || month == 9 || month == 11)
-    {
-        return 30;
-    }
-    else
-    {
-        return 31;
-    }
+string getMonthName(int monthNumber){
+   string months[] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+   return (months[monthNumber]);
 }
-//  For printing the calendar of a given year
-void printCalendar(int year) {
-    cout << "Calendar for year " << year << ":" << endl;
-    cout << "==============================" << endl;
-
-    string months[12] = {"January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"};
-    
-    for (int month = 1; month <= 12; month++) {
-        cout << setw(12) << months[month - 1] << " " << year << endl;
-        cout << " Sun Mon Tue Wed Thu Fri Sat" << endl;
-        // Calculate the starting day of the week for the current month
-        int startDay = (year + (year / 4) - (year / 100) + (year / 400) + ((13 * month + 8) / 5)) % 7;
-        // Print leading spaces
-        for (int i = 0; i < startDay; i++) {
-            cout << "    ";
-        }
-        // Print days
-        int daysInMonth = getDaysInMonth(month, year);
-        for (int day = 1; day <= daysInMonth; day++) {
-            printf("%4d", day);
-            startDay++;
-            if (startDay % 7 == 0 || day == daysInMonth)
-            {
-                cout << endl;
-            }
-        }
-        cout << endl << endl;
-    }
+int numberOfDays (int monthNumber, int year){
+   switch(monthNumber){
+      case 0 :
+      case 2 :
+      case 4 :
+      case 6 :
+      case 7 :
+      case 9 :
+      case 11: return(31);
+      break;
+      case 1 :
+         if (year % 400 == 0 || (year % 4 == 0 && year %100 != 0))
+            return (29);
+         else
+            return (28);
+      break;
+      case 3 :
+      case 5 :
+      case 8 :
+      case 10 : return(30);
+      break;
+   }
 }
-
-int main() {
-    int year = 2024;
-    printCalendar(year);
-    return 0;
+void printCalendar(int year){
+   cout<<"\t\t\t Calendar - Year "<<year;
+   int days;
+   int current = dayNumber (1, 1, year);
+   for (int i = 0; i < 12; i++){
+      days = numberOfDays (i, year);
+      cout<<endl<<"\t\t ----X----"<<getMonthName (i).c_str()<<"----X---- \t\t"<<endl;
+      cout<<" Sun  Mon  Tue  Wed  Thu  Fri  Sat \n";
+      int k;
+      for (k = 0; k < current; k++)
+         cout<<"\t";
+      for (int j = 1; j <= days; j++){
+         printf("%5d", j);
+         if (++k > 6){
+            k = 0;
+            cout<<endl;
+         }
+      }
+      if (k)
+         cout<<endl;
+         current = k;
+      }
+   return;
+}
+int main(){
+   int year = 2019;
+   printCalendar(year);
+   return (0);
 }
